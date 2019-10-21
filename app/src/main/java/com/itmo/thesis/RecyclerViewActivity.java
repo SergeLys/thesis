a033f201bbe5415dd88a634ddbb6c39badae7a3e
lisekno.1996@mail.ru <lisekno.1996@mail.ru>
2019-10-21 18:53:09 +0300

    test images

--- app/src/main/java/com/itmo/thesis/RecyclerViewActivity.java
+++ app/src/main/java/com/itmo/thesis/RecyclerViewActivity.java
@@ -25,7 +25,7 @@ public class RecyclerViewActivity extends AppCompatActivity {
             Type library = (Type) arguments.get("lib");
             Format format = (Format) arguments.get("format");
             imageList = new ArrayList<>();
-            imageList.add(new Image("https://cdn.britannica.com/67/197567-131-1645A26E/Scottish-fold-cat-feline.jpg", getString(R.string.png),"600*800"));
+            imageList.add(new Image("https://raw.githubusercontent.com/SergeLys/findSq/master/images/end_game.jpg", getString(R.string.png),"600*800"));
             RecyclerView recyclerView = findViewById(R.id.list);
             RecyclerView.LayoutManager layoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
             recyclerView.setLayoutManager(layoutManager);
